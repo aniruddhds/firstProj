7a0a24a2240b90891c1adfa9553c03f4e97e3858
D S Anirudha <aniruddhasanjeevi@gmail.com>
2025-11-08 11:00:35 +0530

    added a rudimentary cost calculator at random

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hi');
+function calculate(){
+    const inputEle = document.querySelector('.jsInput');
+
+    let cost = inputEle.value;
+
+    if (cost<40) {
+        cost = cost + 10;
+        
+    }
+
+    document.querySelector('.jsTotalCost').innerHTML = `$${cost}`;
+
+}
