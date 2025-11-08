b9dc59071afe3f750c344b20197433f04cb20b43
D S Anirudha <aniruddhasanjeevi@gmail.com>
2025-11-08 11:21:08 +0530

    code separation into files successfully achieved

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+function handleCostKeydown(event){
+            if (event.key === 'Enter') {
+                calculate();
+            }
+        }
+        
 function calculate(){
     const inputEle = document.querySelector('.jsInput');
 
-    let cost = inputEle.value;
+    let cost = Number(inputEle.value);
 
     if (cost<40) {
-        cost = cost + 10;
-        
+    cost = cost + 10;
     }
 
     document.querySelector('.jsTotalCost').innerHTML = `$${cost}`;
-
 }
